f05a3a90a835f2144b1e2bd76389bfa4ea7b05c7
AhmadAbdallah13 <a7mtoo133@gmail.com>
2022-05-27 03:08:20 +0300

    create the screen and exit functionality

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+
+# initialize the game.
+pygame.init()
+
+# create the screen.
+screen = pygame.display.set_mode((800, 600))
+
+# game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
