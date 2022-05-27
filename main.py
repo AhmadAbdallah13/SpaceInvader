262f9156ca0fc4da5dde4414224d0589ef089bc1
AhmadAbdallah13 <a7mtoo133@gmail.com>
2022-05-27 03:22:21 +0300

    update title, icon, and background color

--- main.py
+++ main.py
@@ -7,10 +7,21 @@ pygame.init()
 # create the screen.
 screen = pygame.display.set_mode((800, 600))
 
+# change title and icon.
+pygame.display.set_caption("Space Invader")
+# todo: icon not working.
+icon = pygame.image.load("spaceship.png")
+pygame.display.set_icon(icon)
+
 # game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # change background color.
+    screen.fill((150, 0, 0))
+
+    pygame.display.update()
 
