44245746076aff078bacb660dfce8a1cf04b2416
AhmadAbdallah13 <a7mtoo133@gmail.com>
2022-06-04 19:57:17 +0300

    improve screen class code

--- game_screen.py
+++ game_screen.py
@@ -2,15 +2,13 @@ import pygame
 
 
 class Screen:
-    # create the screen.
     screen = pygame.display.set_mode((800, 600))
-    # add background image
-    stars_background = pygame.image.load("static/stars.png")
-
-    # change title and icon.
-    pygame.display.set_caption("Space Invader")
-    # todo: icon not working.
-    icon = pygame.image.load("static/spaceship.png").convert()
 
     def __init__(self):
+        self.stars_background = pygame.image.load("static/stars.png")
+        # todo: icon not working.
+        self.icon = pygame.image.load("static/spaceship.png").convert()
+
+        # change title and icon.
+        pygame.display.set_caption("Space Invader")
         pygame.display.set_icon(self.icon)
